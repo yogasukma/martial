5c1eaa578edfd49c596f027305653c393e450285
Yoga Sukma <hello@yogasukma.web.id>
2020-04-25 10:01:07 +0800

    remove placeholder text

--- components/topics-shortcut.php
+++ components/topics-shortcut.php
@@ -9,8 +9,8 @@
 
                 <div class="topics-item">
                     <h3><a href="<?php echo $permalink ?>"><?php echo $category->name ?></a></h3>
-                    <p><?php echo $category->description ?></p>
-                    <p><a href="<?php echo $permalink ?>">Lorem ipsum, dolor sit amet consectetur adipisicing elit. </a></p>
+                    <!-- <p><?php echo $category->description ?></p> -->
+                    <p><a href="<?php echo $permalink ?>"><?php echo $category->description ?></a></p>
                 </div>
 
             <?php
