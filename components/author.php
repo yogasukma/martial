c707d209e13cbcc225d18977b45b149e661144f2
Yoga Sukma <hello@yogasukma.web.id>
2020-04-25 11:01:25 +0800

    wrapping author with p tag

--- components/author.php
+++ components/author.php
@@ -1,10 +1,12 @@
 <div class="author">
 
-    <?php 
-        // @TODO: it should be dynamic, can be configured by admin 
-        $page = get_post(182); 
+    <p>
+        <?php 
+            // @TODO: it should be dynamic, can be configured by admin 
+            $page = get_post(182); 
 
-        echo $page->post_content;
-    ?>
+            echo $page->post_content;
+        ?>
+    </p>
 
 </div>
