a63cc3ec4ec982664d9355cb8b72c9b57b2a798f
Yoga Sukma <hello@yogasukma.web.id>
2020-05-21 08:26:14 +0800

    add twitter image

--- header.php
+++ header.php
@@ -4,5 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
 <?php wp_head(); ?>
+<meta name="twitter:image" content="https://yogasukma.web.id/wp-content/uploads/2020/05/twitter-banner.png">
+
 </head>
 <body <?php body_class() ?>>
